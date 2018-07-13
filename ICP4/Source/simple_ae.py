edaefd01c6f7f7f70c98155a8957c3efb79d5da9
Hari Manasa Ganganagunta <35516506+harimanasag@users.noreply.github.com>
2018-07-12 21:34:02 -0500

    commit

--- ICP4/Source/simple_ae.py
+++ ICP4/Source/simple_ae.py
@@ -20,7 +20,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 #parameters
-logs_path = '/tensorflow_logs'
+logs_path = './summary'
 learning_rate = 0.1
 
 # Variables
